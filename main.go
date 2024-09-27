870013e04539f91db3577bd0985522099dac2bfb
boom.to.dev <boom.to.dev@gmail.com>
2024-09-27 21:22:30 +0700

    commit

--- main.go
+++ main.go
@@ -4,8 +4,12 @@ import (
 	"bufio"
 	"encoding/csv"
 	"fmt"
+	"log"
 	"os"
+	"sort"
+	"strconv"
 	"strings"
+	"unicode/utf8"
 )
 
 type Replacement struct {
@@ -13,20 +17,53 @@ type Replacement struct {
 	Replacement string
 }
 
+type DictionaryWord struct {
+	Word      string
+	Translate string
+	Length    int
+}
+
+func sortCsv(csvTxt [][]string) []DictionaryWord {
+
+	var DictionaryWordList []DictionaryWord
+	for _, record := range csvTxt {
+		if len(record) >= 2 {
+			dictionaryWord := DictionaryWord{
+				Word:      strings.TrimSpace(record[0]),
+				Translate: strings.TrimSpace(record[1]),
+				Length:    utf8.RuneCountInString(strings.TrimSpace(record[0])),
+			}
+			DictionaryWordList = append(DictionaryWordList, dictionaryWord)
+		}
+	}
+
+	sort.SliceStable(DictionaryWordList, func(i, j int) bool {
+		return DictionaryWordList[i].Length > DictionaryWordList[j].Length
+	})
+
+	return DictionaryWordList
+}
+
 func readReplacementsFromCSV(filePath string) ([]Replacement, error) {
+
 	var replacements []Replacement
 
 	file, err := os.Open(filePath)
 	if err != nil {
 		return nil, err
+	} else {
+		defer file.Close()
 	}
-	defer file.Close()
 
 	reader := csv.NewReader(file)
 	records, err := reader.ReadAll()
 	if err != nil {
 		return nil, err
 	}
+
+	DictionaryWordList := sortCsv(records)
+
+	CsvNewWriter("replacementSort.csv", DictionaryWordList)
 
 	for _, record := range records {
 		if len(record) >= 2 {
@@ -71,6 +108,37 @@ func replaceTextInFile(inputFilePath, outputFilePath string, replacements []Repl
 	}
 
 	return nil
+}
+
+func CsvNewWriter(path string, DictionaryWordList []DictionaryWord) {
+	csvFile, err := os.Create(path)
+	if err != nil {
+		log.Fatalf("failed creating file: %s", err)
+	}
+	bomUtf8 := []byte{0xEF, 0xBB, 0xBF}
+	csvFile.Write(bomUtf8)
+	csvWriter := csv.NewWriter(csvFile)
+
+	defer csvFile.Close()
+	r := make([]string, 0, 3)
+	r = append(r, "Word")
+	r = append(r, "Translate")
+	r = append(r, "Length")
+	csvWriter.Write(r)
+
+	for _, DictionaryWord := range DictionaryWordList {
+		r := make([]string, 0, 3)
+
+		r = append(r, DictionaryWord.Word)
+		r = append(r, DictionaryWord.Translate)
+		r = append(r, strconv.Itoa(DictionaryWord.Length))
+		err := csvWriter.Write(r)
+		if err != nil {
+			panic(err)
+		}
+		csvWriter.Flush()
+	}
+
 }
 
 func main() {
